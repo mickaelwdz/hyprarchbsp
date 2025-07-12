91971f3d804fca28975502eda0df342326d68e06
MickaelWDZ <131960663+mickaelwdz@users.noreply.github.com>
2025-07-12 07:52:47 +0200

    Update packages.py

--- Builder/packages.py
+++ Builder/packages.py
@@ -6,12 +6,12 @@ BASE = Packages(
 			"pacman-contrib", "downgrade", "libnotify", "ffmpeg","ffmpegthumbnailer", "jq", "parallel", "kitty", "fastfetch", "lsd", "bat", "brightnessctl", 
 			"automake", "blueman", "bluez", "bluez-utils", "dunst", "fakeroot", "firefox", "fish", "fisher", "dpkg", "gcc", "git", "gnu-netcat", "btop", 
 			"micro", "mat2", "nemo", "papirus-icon-theme", "pavucontrol", "pamixer", "pipewire", "pipewire-pulse", "pipewire-audio",
-			"pipewire-jack", "pipewire-alsa", "wireplumber", "python-pyalsa", "ranger", "redshift", "reflector", "sudo", "tree", "unrar",
-			"zip", "unzip", "uthash", "ark", "cmake", "clang", "gzip", "imagemagick",
+			"pipewire-jack", "pipewire-alsa", "wireplumber", "python-pyalsa", "redshift", "reflector", "sudo", "tree", "unrar",
+			"zip", "unzip", "uthash", "ark", "cmake", "clang", "gzip", "imagemagick", "7zip", "poppler", "fd", "ripgrep", "fzf", "zoxide", 
 			"make", "openssh", "shellcheck", "mpv", "loupe", "usbutils", "openvpn", "networkmanager-openvpn", "p7zip", "gparted",
 			"sshfs", "wget", "netctl", "gedit", "ttf-jetbrains-mono", "ttf-jetbrains-mono-nerd", "ttf-fira-code",
         	"ttf-iosevka-nerd", "playerctl", "starship", "upower", "udiskie", "zenity", "gvfs", "qt5ct", "qt6ct",
-        	"timeshift", "sddm", "qt5-graphicaleffects", "qt5-svg",  "qt5-quickcontrols2", "clipnotify", "yt-dlp",
+        	"timeshift", "sddm", "qt5-graphicaleffects", "qt5-svg",  "qt5-quickcontrols2", "clipnotify", "yt-dlp", "termusic", 
 			"xdg-desktop-portal-gtk", "gnome-disk-utility", "evince", "neovim", "tmux", "cowsay", "polkit-gnome",
 			"rofimoji", "wmname", "pyenv", "xdg-desktop-portal", "ttf-hack-nerd", "networkmanager", "noto-fonts", 
             "noto-fonts-cjk", "noto-fonts-emoji", "noto-fonts-extra"
@@ -26,7 +26,7 @@ BASE = Packages(
 			"gnome-calculator-gtk3", "flameshot-git", "rofi-lbonn-wayland-git", "bibata-cursor-theme-bin", "tela-circle-icon-theme-dracula",
 			"themix-theme-oomox-git", "themix-plugin-base16-git", "themix-gui-git", "themix-export-spotify-git",
 			"themix-theme-materia-git", "oomox-qt5-styleplugin-git", "oomox-qt6-styleplugin-git", "cava", "pokemon-colorscripts-git",
-			"update-grub", "ttf-meslo-nerd-font-powerlevel10k"
+			"update-grub", "yazi-git", "resvg", "ttf-meslo-nerd-font-powerlevel10k"
 		],
 		bspwm_packages=["i3lock-color", "picom-ftlabs-git"],
 		hyprland_packages=["hyprpicker", "swaylock-effects-git", "wlr-randr-git", "hyprprop", "grimblast-git"]
